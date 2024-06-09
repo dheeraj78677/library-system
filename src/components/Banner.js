c26f6377cb6b6e9e42eb160faa1fa26ee69cab92
Dheeraj <dheerajsingh78677@gmail.com>
2024-06-09 11:47:25 +1000

    push

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import SignupModal from './SignUpModal';
 import LoginModal from './LoginModal';
 import './Banner.css';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import LoginModal from './LoginModal';
 import SignUpModal from './SignUpModal';
 import './Navbar.css';

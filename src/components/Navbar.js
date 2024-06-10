cb580b3d441089b820313e36fc475de61dcc262a
Dheeraj <dheerajsingh78677@gmail.com>
2024-06-10 23:21:32 +1000

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,28 +9,23 @@ const Navbar = ({ isLoggedIn, onLogout, onEditProfile, onResetPassword, openLogi
   console.log('isLoggedIn ',isLoggedIn);
   return (
     <div className="navbar">
-      <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="RMIT Logo" className="navbar-logo" />
-      <h1 className="navbar-title">RMIT LIBRARY</h1>
-      <div className="navbar-buttons"></div>
-      <div className="navbar-buttons">
-      {isLoggedIn && (
-            <div>
-            <button className="navbar-button" onClick={onEditProfile}>Edit Profile</button>
-            <button className="navbar-button" onClick={onResetPassword}>Reset Password</button>
-            <button className="navbar-button" onClick={onLogout}>Logout</button>
-            </div>
-             )}
-
-      {!isLoggedIn && (
-            <div>
-             <button className="navbar-button" onClick={openLoginModal}>Login</button>
-             <button className="navbar-button" onClick={openSignupModal}>Sign Up</button>
-            </div>
-       )}
-            
-      </div>
-  
+    <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="RMIT Logo" className="navbar-logo" />
+    <h1 className="navbar-title">RMIT LIBRARY</h1>
+    <div className="navbar-buttons">
+      {isLoggedIn ? (
+        <>
+          <button className="navbar-button" onClick={onEditProfile}>Edit Profile</button>
+          <button className="navbar-button" onClick={onResetPassword}>Reset Password</button>
+          <button className="navbar-button" onClick={onLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <button className="navbar-button" onClick={openLoginModal}>Login</button>
+          <button className="navbar-button" onClick={openSignupModal}>Sign Up</button>
+        </>
+      )}
     </div>
+  </div>
   );
 };
 
